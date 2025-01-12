9651463fbcd853817f89bf74a77806348fba94fc
VishalKarnati <vishalkarnati9@gmail.com>
2025-01-12 13:51:21 +0530

    Remove src/queries directory from Git tracking

--- src/queries/customers.js
+++
@@ -1,9 +0,0 @@
-import { Customer } from "@/model/customer-model";
-
-export async function createCustomer(customer){
-    try {
-        await Customer.create(customer);
-    } catch (error) {
-        throw new Error(error);
-    }
-}
